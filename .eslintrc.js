79e32c35e59c14e60d95f5d841a8be2d91fe55c8
Mateusz <bloxorzykigi@gmail.com>
2022-10-05 20:58:47 +0200

    fix: eslint conflicts

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,99 +1,26 @@
 module.exports = {
-  root: true,
   env: {
+    es6: true,
     browser: true,
     es2021: true
   },
-  parser: "@typescript-eslint/parser",
-  plugins: [
-    "react",
-    "@typescript-eslint",
-    "react-hooks",
-    "prettier"
-  ],
   extends: [
-    "airbnb",
-    "plugin:react/recommended",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:import/errors",
-    "plugin:import/warnings",
-    "plugin:import/typescript",
-    "plugin:prettier/recommended"
+    'plugin:react/recommended',
+    'standard-with-typescript'
+  ],
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: ['tsconfig.json']
+  },
+  plugins: [
+    'react',
+    '@typescript-eslint'
   ],
   rules: {
-    "react/react-in-jsx-scope": "off",
-    "import/no-unresolved": "error",
-    "react-hooks/rules-of-hooks": "error",
-    "react-hooks/exhaustive-deps": "error",
-    "react/jsx-filename-extension": [
-      1,
-      {
-        extensions: [
-          ".tsx"
-        ]
-      }
-    ],
-    "import/prefer-default-export": "off",
-    "import/extensions": [
-      "error",
-      "ignorePackages",
-      {
-        ts: "never",
-        tsx: "never"
-      }
-    ],
-    "@typescript-eslint/no-unused-expressions": [
-      "error",
-      {
-        allowTernary: true
-      }
-    ],
-    "react/prop-types": "off",
-    "import/no-cycle": "off",
-    "no-shadow": "off",
-    "@typescript-eslint/ban-ts-comment": "off",
-    "@typescript-eslint/no-shadow": ["error"],
-    "@typescript-eslint/explicit-module-boundary-types": "off",
-    "react/jsx-one-expression-per-line": "off",
-    "react/jsx-key": "warn",
-    "no-use-before-define": "off",
-    "@typescript-eslint/no-use-before-define": "off",
-    "react/jsx-props-no-spreading": "off",
-    "react/jsx-max-props-per-line": [1, { "when": "multiline" }],
-    "no-extra-semi": "warn",
-    "no-irregular-whitespace": "warn",
-    "no-empty-pattern": "warn",
-    "react/jsx-closing-bracket-location": "warn",
-    "react/jsx-curly-spacing": "warn",
-    "react/jsx-indent-props": "warn",
-    "react/jsx-indent": "warn",
-    "react/jsx-tag-spacing": "warn",
-    "react/no-unused-state": "warn",
-    "react/jsx-closing-tag-location": "warn",
-    "max-len": ["warn", { "code": 120 }],
-    "import/order": "warn",
-    "arrow-parens": "warn",
-    "no-multiple-empty-lines": "warn",
-    "no-empty-function:": "off",
-    "@typescript-eslint/no-empty-function": "off",
-    "comma-dangle": "warn",
-    "indent": "off", // done by prettier
-    "semi": "warn",
-    "spaced-comment": "warn",
-    "no-trailing-spaces": "warn",
-    "camelcase": "off"
-  },
-  settings: {
-    "import/resolver": {
-      "node": {
-        "moduleDirectory": ["node_modules", "src/"]
-      }
-    }
-  },
-  ignorePatterns: [
-    "node_modules",
-    "**/*.stories.tsx",
-    "**/.eslintrc.js",
-    "craco.config.js",
-  ]
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/naming-convention': 'off'
+
+  }
 }
